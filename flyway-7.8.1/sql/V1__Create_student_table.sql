bd4f91b0c296a7e3962034d51a95b4f9e6896abc
ekaterina-abr <ekaterina.abr.99@mail.ru>
2021-04-22 22:03:38 +0300

    Added flyway files

---
+++ flyway-7.8.1/sql/V1__Create_student_table.sql
@@ -0,0 +1,5 @@
+create table STUDENT (
+      ID int not null,
+      NAME varchar(100) not null,
+      SURNAME varchar(100) not null
+);
---
+++ flyway-7.8.1/sql/V2__Add_students.sql
@@ -0,0 +1,3 @@
+insert into STUDENT (ID, NAME, SURNAME) values (1, 'Alexey', 'Kozhevnikov');
+insert into STUDENT (ID, NAME, SURNAME) values (2, 'Elizaveta', 'Dobraya');
+insert into STUDENT (ID, NAME, SURNAME) values (3, 'Petr', 'Ivanov');
